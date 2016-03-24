84cd337622a4b597d0db8faa6d0bd024157a281b
MrVirus <506848309@qq.com>
2016-03-24 10:27:14 +0800

    Android

---
+++ game2048/gen/com/example/sun/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.sun;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ game2048/src/com/example/sun/Const.java
@@ -0,0 +1,15 @@
+package com.example.sun;
+
+/**
+ * Created by Ho on 2014/8/13.
+ */
+public class Const {
+    // 广告应用ID
+    public final static String COOID = "db047382a6b94e719cab110ea52c29b9";
+    // 百度 渠道ID
+    public final static String BAIDU_CHID = "k-baidu";
+    // 腾讯
+    public final static String QQ_CHID = "k-3gqq";
+    // 木蚂蚁
+    public final static String MMY_CHID = "k-mumayi";
+}
